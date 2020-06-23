251245b3279f3ecbd0e5491131136113d6218077
sahapro33 <sahapro33@users.noreply.github.com>
2020-06-23 23:00:15 +0300

    asdasd

---
+++ js/1.js
@@ -0,0 +1,66 @@
+function click1(){
+    document.getElementById('c1').style.color = 'rgb(247,195,8)'; 
+    document.getElementById('c2').style.color = 'white'; 
+    document.getElementById('c3').style.color = 'white'; 
+    document.getElementById('c4').style.color = 'white'; 
+    document.getElementById('c5').style.color = 'white';
+
+    document.getElementById('Magazin').style.display = 'block';  
+    document.getElementById('o_nas').style.display = 'none'; 
+    document.getElementById('gde_ky').style.display = 'none'; 
+    document.getElementById('FAQ').style.display = 'none'; 
+    document.getElementById('cont').style.display = 'none'; 
+
+}
+function click2(){
+    document.getElementById('c1').style.color = 'white'; 
+    document.getElementById('c2').style.color = 'rgb(247,195,8)'; 
+    document.getElementById('c3').style.color = 'white'; 
+    document.getElementById('c4').style.color = 'white'; 
+    document.getElementById('c5').style.color = 'white'; 
+
+    document.getElementById('Magazin').style.display = 'none';  
+    document.getElementById('o_nas').style.display = 'block'; 
+    document.getElementById('gde_ky').style.display = 'none'; 
+    document.getElementById('FAQ').style.display = 'none'; 
+    document.getElementById('cont').style.display = 'none'; 
+}
+function click3(){
+    document.getElementById('c1').style.color = 'white'; 
+    document.getElementById('c2').style.color = 'white'; 
+    document.getElementById('c3').style.color = 'rgb(247,195,8)'; 
+    document.getElementById('c4').style.color = 'white'; 
+    document.getElementById('c5').style.color = 'white'; 
+
+    document.getElementById('Magazin').style.display = 'none';  
+    document.getElementById('o_nas').style.display = 'none'; 
+    document.getElementById('gde_ky').style.display = 'block'; 
+    document.getElementById('FAQ').style.display = 'none'; 
+    document.getElementById('cont').style.display = 'none'; 
+}
+function click4(){
+    document.getElementById('c1').style.color = 'white'; 
+    document.getElementById('c2').style.color = 'white'; 
+    document.getElementById('c3').style.color = 'white'; 
+    document.getElementById('c4').style.color = 'rgb(247,195,8)'; 
+    document.getElementById('c5').style.color = 'white'; 
+
+    document.getElementById('Magazin').style.display ='none' ;  
+    document.getElementById('o_nas').style.display = 'none'; 
+    document.getElementById('gde_ky').style.display = 'none'; 
+    document.getElementById('FAQ').style.display = 'block'; 
+    document.getElementById('cont').style.display = 'none'; 
+}
+function click5(){
+    document.getElementById('c1').style.color = 'white'; 
+    document.getElementById('c2').style.color = 'white'; 
+    document.getElementById('c3').style.color = 'white'; 
+    document.getElementById('c4').style.color = 'white'; 
+    document.getElementById('c5').style.color = 'rgb(247,195,8)'; 
+
+    document.getElementById('Magazin').style.display = 'none';  
+    document.getElementById('o_nas').style.display = 'none'; 
+    document.getElementById('gde_ky').style.display = 'none'; 
+    document.getElementById('FAQ').style.display = 'none'; 
+    document.getElementById('cont').style.display = 'block'; 
+}
